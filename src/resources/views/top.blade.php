0a169fd160a0a8b2b0d7b4be12819ee2c51d19b8
白金愛里 <air.shirogane@gmail.com>
2025-03-17 05:41:30 +0900

    初回コミット

---
+++ src/resources/views/top.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="テキストテキストテキスト">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/favicon.ico">
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/top.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}" />
+    <title>Web Entertainment Design </title>
+</head>
+<body>
+
+</body>
+</html>
